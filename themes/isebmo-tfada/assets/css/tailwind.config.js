0bd11ca8b3f5f36f64d0ca3ff886dcb6ad32a4ef
Sébastien Mouret <sebastienmouret@MacBook-Pro.local>
2020-08-13 11:21:34 +0200

    dark mode support !

--- themes/isebmo-tfada/assets/css/tailwind.config.js
+++ themes/isebmo-tfada/assets/css/tailwind.config.js
@@ -1,10 +1,16 @@
 
 module.exports = {
     theme: {
+        extend: {
+            screens: {
+                dark: { raw: "(prefers-color-scheme: dark)" }
+            }
+        },
         fontFamily: {
             sans: ['"Work Sans"']
-        }
+        },
     },
     variants: {},
-    plugins: []
+    plugins: [
+    ]
 }
